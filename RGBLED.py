66d3faacee16d0f6bb057c4d20ecd9b3f313d8fb
Tohir_kimsanboy <skillsme22@gmail.com>
2022-05-15 18:57:48 +0900

    add RGBLED code

---
+++ RGBLED.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(11,GPIO.OUT)
+GPIO.output(11,1)
+GPIO.setup(13,GPIO.OUT)
+GPIO.output(13,1)
+GPIO.setup(15,GPIO.OUT)
+GPIO.output(15,1)
+
+try:
+    while(True):
+        request = raw_input("RGB-->")
+        
+except KeyboardInterrupt:
+    GPIO.cleanup()
