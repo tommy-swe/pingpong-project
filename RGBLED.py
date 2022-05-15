c670b64193bd03d0076bc901c33fd409ac6280d9
Tohir_kimsanboy <skillsme22@gmail.com>
2022-05-15 19:09:47 +0900

    fix RGBLED

--- RGBLED.py
+++ RGBLED.py
@@ -2,16 +2,20 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(11,GPIO.OUT)
-GPIO.output(11,1)
-GPIO.setup(13,GPIO.OUT)
-GPIO.output(13,1)
-GPIO.setup(15,GPIO.OUT)
-GPIO.output(15,1)
+GPIO.setup(16,GPIO.OUT)
+GPIO.output(16,1)
+GPIO.setup(20,GPIO.OUT)
+GPIO.output(20,1)
+GPIO.setup(21,GPIO.OUT)
+GPIO.output(21,1)
 
 try:
     while(True):
         request = input("RGB-->")
+        if (len(request) == 3):
+            GPIO.output(16, int(request[0]))
+            GPIO.output(20, int(request[1]))
+            GPIO.output(21, int(request[2]))
         
 except KeyboardInterrupt:
     GPIO.cleanup()
