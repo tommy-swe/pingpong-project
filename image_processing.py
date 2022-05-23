4f603cc10170f2b89f6e6a240ef075d28c797e0d
Kaori <dtvu1707@gmail.com>
2022-05-23 23:10:56 +0900

    add ball detection

--- image_processing.py
+++ image_processing.py
@@ -1,3 +1,6 @@
+from json.tool import main
+from tkinter.messagebox import NO
+from typing import final
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -5,47 +8,106 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib import colors
 
-cap = cv2.VideoCapture('video_data/twosize_5.mp4')
+cap = cv2.VideoCapture('video_data/half_table_middle.mp4')
 fps = cap.get(cv2.CAP_PROP_FPS)
 print("Frame rate: ", int(fps), "FPS")
 
 
-def segment_by_color(img):
-    hei, wid= img.shape[:2]
+def detect_table_Color(img):
+
+   
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     h, s, v = cv2.split(hsv)
-
-    #ball segmentation
-    light_orange = (1, 150, 150)
-    dark_orange = (15, 255, 255)
-    mask_orange = cv2.inRange(hsv, light_orange, dark_orange)
-
     #table segmentation
     light_blue = (110, 100, 100)
     dark_blue = (150, 255, 255)
     
     mask_blue = cv2.inRange(hsv, light_blue, dark_blue)
-    final_mask = mask_orange
+  
+    # final_mask = mask_orange + mask_blue #segment table and ball
+    final_mask = mask_blue # segment only table
+   
+    final_mask = cv2.medianBlur(final_mask, ksize = 9)
 
     result = cv2.bitwise_and(img, img, mask=final_mask)
-    return result, final_mask
 
-def bigget_countours(img, mask):
-
-    ret, thresh = cv2.threshold(mask, 40, 255, 0)     
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    #draw contours        
+    contours, hierarchy = cv2.findContours(final_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     
     if len(contours) != 0:
         # draw in blue the contours that were founded
-        cv2.drawContours(output, contours, -1, 255, 3)
+        cv2.drawContours(result, contours, -1, (255, 255, 0), 3)
 
         # find the biggest countour (c) by the area
         c = max(contours, key = cv2.contourArea)
+        cv2.drawContours(result, [c], 0, (255, 0, 255), 3)
         x,y,w,h = cv2.boundingRect(c)
 
     # draw the biggest contour (c) in green
-    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-    return img
+    cv2.rectangle(result,(x,y),(x+w,y+h),(0,255,0),2)
+
+  
+    return result, final_mask, [x, y, w, h]
+
+
+def ball_in_table_detect(img, mask, bg_mask):
+
+    output = img.copy()
+    ball_segment = mask - bg_mask
+    contours, hierarchy = cv2.findContours(ball_segment, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    if len(contours) != 0:
+        # find the biggest countour (c) by the area
+        cv2.drawContours(output, contours, -1, (255, 255, 0), 3)
+        c = max(contours, key = cv2.contourArea)
+        cv2.drawContours(output, [c], 0, (0, 0, 255), -1)
+        x,y,w,h = cv2.boundingRect(c)
+
+    return output
+
+def detect_ball_Houghtransform(img, mask):
+
+    # Creating kernel
+    kernel = np.ones((7, 7), np.uint8)
+
+    output = img.copy()
+    ball_segment = mask.copy()
+    ball_segment = cv2.erode(ball_segment, kernel)
+
+    detected_circles = cv2.HoughCircles(ball_segment, 
+                   cv2.HOUGH_GRADIENT, 1, 50, param1 = 80,
+               param2 = 10, minRadius = 1, maxRadius = 50)
+
+    c = []
+    if detected_circles is not None:
+        # Convert the circle parameters a, b and r to integers.
+        detected_circles = np.uint16(np.around(detected_circles))
+    
+        detected_circles = detected_circles[0, :]
+        detected_circles = detected_circles[detected_circles[:, 2].argsort()]
+        for pt in detected_circles:
+            a, b, r = pt[0], pt[1], pt[2]
+            # Draw the circumference of the circle.
+            cv2.circle(output, (a, b), r, (255, 255, 0), -1)
+        
+      
+        c = detected_circles[-1]
+        cv2.circle(output, (c[0], c[1]), c[2], (0, 0, 255), -1)
+
+    return output, c
+
+def draw_trajectory(img, trajectory):
+     
+    output = img.copy()
+    n = len(trajectory)
+    for i in range(n - 5, n):
+        a1, b1, r1 = trajectory[i]
+        a2, b2, r2 = trajectory[i+1]
+        cv2.circle(output, (a1, b1), 1, (255, 255, 255), -1)
+        cv2.line(output, (a1, b1), (a2, b2), (125, 125, 125), 2)
+    
+    return output
+
 
 
 def draw_color_hist(img):
@@ -67,24 +129,34 @@ def draw_color_hist(img):
     # plt.pause(0.1)
 
 
-size = (640, 480)
-fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
-output = cv2.VideoWriter('twosize_5.avi', fourcc, fps, size)
+if __name__ == "__main__":
 
-try:
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        
-        re_frame = cv2.resize(frame, size, interpolation = cv2.INTER_AREA)
-        out, mask = segment_by_color(re_frame)
-        # table_with_contours = bigget_countours(out, mask)
-        # draw_color_hist(out)
+    size = (640, 480)
+    fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+    # output = cv2.VideoWriter('half_table_middle.avi', fourcc, fps, size)
+    output = cv2.VideoWriter('test.avi', fourcc, 10, size)
+    flag = 0
+    trajectory = []
+    try:
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+           
+            re_frame = cv2.resize(frame, size, interpolation = cv2.INTER_AREA)
+            out, mask, rec = detect_table_Color(re_frame)
+            if(flag == 0):
+                mask_bg = mask
+                flag += 1
+            table_with_ball, c = detect_ball_Houghtransform(out, mask)
+            # draw_color_hist(out)
+            if(len(c) == 0):
+                trajectory.append(c)
 
-        cv2.imshow('frame', out)
-        #   cv2.waitKey(10)
-        if cv2.waitKey(0) & 0xFF == ord('q'):
-            break
-        # output.write(out)
-except:
-    cap.release()
-    cv2.destroyAllWindows()
+            cv2.imshow('contours',  table_with_ball)
+            cv2.imshow('frame', mask)
+            #   cv2.waitKey(10)
+            if cv2.waitKey(0) & 0xFF == ord('q'):
+                break
+            output.write(table_with_ball)
+    except:
+        cap.release()
+        cv2.destroyAllWindows()
