d6c851a92b03c60650e2a8485f9f47e4af353a60
Kaori <dtvu1707@gmail.com>
2022-06-11 15:52:47 +0900

    LED

--- RGBLED_test.py
+++ RGBLED_test.py
@@ -71,13 +71,13 @@ def destroy():
 	# Release resource
 	GPIO.cleanup()
 
-# If run this script directly, do:
-if __name__ == '__main__':
-	setup()
-	try:
-		main()
-	# When 'Ctrl+C' is pressed, the program 
-	# destroy() will be  executed.
-	except KeyboardInterrupt:
+# # If run this script directly, do:
+# if __name__ == '__main__':
+# 	setup()
+# 	try:
+# 		main()
+# 	# When 'Ctrl+C' is pressed, the program 
+# 	# destroy() will be  executed.
+# 	except KeyboardInterrupt:
         
-		destroy()
+# 		destroy()
