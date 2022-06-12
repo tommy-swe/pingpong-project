f3b134b61dbc84fb58eaec2de2b344ad9bd299c6
Kaori <dtvu1707@gmail.com>
2022-06-12 21:49:09 +0900

    update main

--- main_code/main.py
+++ main_code/main.py
@@ -37,55 +37,55 @@ COLOR = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF00FF, 0x00FFFF]
 #     except KeyboardInterrupt:
 #         controller.reset()
 
-def main():
-    controller_LED = RGBLEDcontroller()
-    controller_LED.setup()
-    controller_Digit = DigitLEDcontrolller(clk=3, dio=2)
+# def main():
+#     controller_LED = RGBLEDcontroller()
+#     controller_LED.setup()
+#     controller_Digit = DigitLEDcontrolller(clk=3, dio=2)
 
-    for video in (os.listdir("test")):
+#     for video in (os.listdir("test")):
 
-        #start recording:
-        controller_LED.setColor(COLOR[0])
+#         #start recording:
+#         controller_LED.setColor(COLOR[0])
 
-        cap = cv2.VideoCapture(os.path.join('test', video))
-        fps = int(cap.get(cv2.CAP_PROP_FPS))
-        print("Video name: ", video)
-        print("Frame rate: ", fps, "FPS")
+#         cap = cv2.VideoCapture(os.path.join('test', video))
+#         fps = int(cap.get(cv2.CAP_PROP_FPS))
+#         print("Video name: ", video)
+#         print("Frame rate: ", fps, "FPS")
 
-        score_board = PingPongAlg(fps = fps, isshow=False, isdraw=False)
+#         score_board = PingPongAlg(fps = fps, isshow=False, isdraw=False)
 
-        try:
-            while(cap.isOpened()):
-                ret, frame = cap.read()
+#         try:
+#             while(cap.isOpened()):
+#                 ret, frame = cap.read()
 
-                # table detection
-                score_board.table_detection(frame=frame)
-                # ball detection
-                score_board.ball_detection()
+#                 # table detection
+#                 score_board.table_detection(frame=frame)
+#                 # ball detection
+#                 score_board.ball_detection()
                 
-                score_board.scoring()
-                l_score, r_score = score_board.score["L"], score_board.score["R"]
-                print(l_score, r_score)
-                controller_Digit.setNumber(l_score, r_score)
+#                 score_board.scoring()
+#                 l_score, r_score = score_board.score["L"], score_board.score["R"]
+#                 print(l_score, r_score)
+#                 controller_Digit.setNumber(l_score, r_score)
 
-                if(score_board.isshow == True):
-                    cv2.imshow('contours',  score_board.result_img)
-                    # cv2.imshow('frame', score_board.curent_frame)
-                    cv2.waitKey(5)
+#                 if(score_board.isshow == True):
+#                     cv2.imshow('contours',  score_board.result_img)
+#                     # cv2.imshow('frame', score_board.curent_frame)
+#                     cv2.waitKey(5)
 
-                if 0xFF == ord('q'):
-                    break
-        except:
-            cap.release()
-            cv2.destroyAllWindows()
-            controller_LED.stop()
-            controller_Digit.reset()
+#                 if 0xFF == ord('q'):
+#                     break
+#         except:
+#             cap.release()
+#             cv2.destroyAllWindows()
+#             controller_LED.stop()
+#             controller_Digit.reset()
 
-        # finally:
-        #     cap.release()
-        #     cv2.destroyAllWindows()
-        #     controller_LED.stop()
-        #     controller_Digit.reset()
+#         # finally:
+#         #     cap.release()
+#         #     cv2.destroyAllWindows()
+#         #     controller_LED.stop()
+#         #     controller_Digit.reset()
 
 
 def main():
@@ -95,8 +95,9 @@ def main():
     controller_Digit = DigitLEDcontrolller(clk=3, dio=2)
 
     while(True):
-
+        
         cam = cv2.VideoCapture(0)
+
         if (cam.isOpened() == False): 
             print("Error reading video file")
             controller_LED.setColor(COLOR[1])
@@ -107,6 +108,7 @@ def main():
 
         else:
             #start recording:
+            st_time = time.time()
             controller_LED.setColor(COLOR[0])
             fps = int(cam.get(cv2.cam_PROP_FPS))
             print("Frame rate: ", fps, "FPS")
@@ -116,28 +118,33 @@ def main():
                 size = score_board.base_size
                 fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
                 # fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-                output = cv2.VideoWriter("test.avi", fourcc, fps, size)
+                output = cv2.VideoWriter(str(int(st_time)) + ".avi", fourcc, fps, size)
 
 
             while(True):
                 ret, frame = cam.read()
 
+                if(int(time.time() - st_time) > 5 * 60): #new record after n seconds
+                    break
+
                 if(ret == True):
                     
-                    # table detection
-                    score_board.table_detection(frame=frame)
-                    # ball detection
-                    score_board.ball_detection()
-                    
-                    score_board.scoring()
-                    l_score, r_score = score_board.score["L"], score_board.score["R"]
-                    print(l_score, r_score)
-                    controller_Digit.setNumber(l_score, r_score)
+                    # # table detection
+                    # score_board.table_detection(frame=frame)
+                    # # ball detection
+                    # score_board.ball_detection()
+                    # #ball scoring
+                    # score_board.scoring()
+                    # l_score, r_score = score_board.score["L"], score_board.score["R"]
+                    # print(l_score, r_score)
+                    # controller_Digit.setNumber(l_score, r_score)
 
-                    if(score_board.isshow == True):
-                        cv2.imshow('contours',  score_board.result_img)
-                        # cv2.imshow('frame', score_board.curent_frame)
-                        cv2.waitKey(1)
+                    # if(score_board.isshow == True):
+                    #     cv2.imshow('contours',  score_board.result_img)
+                    #     # cv2.imshow('frame', score_board.curent_frame)
+                    #     cv2.waitKey(1)
+
+                    cv2.imshow('frame', frame)
 
                     if cv2.waitKey(1) & 0xFF == ord('s'):
                         break
