31c22e0e7870db106e8586e7fd9132472829c6ce
Kaori <dtvu1707@gmail.com>
2022-06-12 21:13:42 +0900

    opencv_camera

--- opencv_camera.py
+++ opencv_camera.py
@@ -14,13 +14,15 @@ else:
     frame_height = int(cam.get(4))
     size = (frame_width, frame_height)
     fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+    # fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     output = cv2.VideoWriter("test.avi", fourcc, 10, size)
 
     while True:
         ret, image = cam.read()
+        
         cv2.imshow('Imagetest',image)
-        k = cv2.waitKey(1)
-        if k != -1:
+
+        if cv2.waitKey(1) & 0xFF == ord('s'):
             break
         output.write(image)
 
