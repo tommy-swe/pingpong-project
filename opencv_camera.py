f06480c033555082afec08c6486ed6ea690fccfb
Kaori <dtvu1707@gmail.com>
2022-06-12 21:10:48 +0900

    opencv camera

--- opencv_camera.py
+++ opencv_camera.py
@@ -1,13 +1,28 @@
 import cv2
+import os
 
 cam = cv2.VideoCapture(0)
+fps = int(cam.get(cv2.CAP_PROP_FPS))
+print("Frame rate: ", fps, "FPS")
 
-while True:
-	ret, image = cam.read()
-	cv2.imshow('Imagetest',image)
-	k = cv2.waitKey(1)
-	if k != -1:
-		break
-cv2.imwrite('/home/pi/testimage.jpg', image)
-cam.release()
-cv2.destroyAllWindows()
+
+if (cam.isOpened() == False): 
+    print("Error reading video file")
+
+else:
+    frame_width = int(cam.get(3))
+    frame_height = int(cam.get(4))
+    size = (frame_width, frame_height)
+    fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+    output = cv2.VideoWriter("test.avi", fourcc, 10, size)
+
+    while True:
+        ret, image = cam.read()
+        cv2.imshow('Imagetest',image)
+        k = cv2.waitKey(1)
+        if k != -1:
+            break
+        output.write(image)
+
+    cam.release()
+    cv2.destroyAllWindows()
